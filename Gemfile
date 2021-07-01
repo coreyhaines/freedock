cb8f41c507d6680ab0167524a005651ba64da67b
Corey Haines <coreyhaines@gmail.com>
2021-07-01 13:46:35 -0500

    switch to http gem https://github.com/httprb/http

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'json'
 gem 'hotwire-rails'
+gem 'http'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/services/divvy_api.rb
+++ app/services/divvy_api.rb
@@ -1,18 +1,13 @@
-require 'net/http'
-require 'json'
+require 'http'
 
 class DivvyApi
   def self.all_station_information
     url = "https://gbfs.divvybikes.com/gbfs/en/station_information.json"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    JSON.parse(response).dig("data", "stations")
+    HTTP.get(url).parse.dig("data", "stations")
   end
 
   def self.all_station_status
     url = "https://gbfs.divvybikes.com/gbfs/en/station_status.json"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    JSON.parse(response).dig("data", "stations")
+    HTTP.get(url).parse.dig("data", "stations")
   end
 end
